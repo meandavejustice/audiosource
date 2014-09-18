45772742420adbeb427e52e9d91cbbe20ad181d3
David Adam Justice <david@outthinkgroup.com>
2014-09-17 23:04:54 -0700

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# AudioSource
+
+A simple utility to manage audio buffers.
+
+takes an audio context, url, gainNode, and an array of [ffts](https://github.com/meandavejustice/audio-fft)
+
+Handy methods include:
+* getBuffer
+* getSource
+* disconnect
+* play
+* loadSilent
+* stop
+* decode
---
+++ index.js
@@ -0,0 +1,101 @@
+/*
+ * AudioSource
+ *
+ * * MUST pass an audio context
+ *
+ */
+function AudioSource (context, opts) {
+  if (!context) {
+    throw new Error('You must pass an audio context to use this module');
+  }
+
+  this.context = context;
+  this.buffer = undefined;
+  this.url = opts.url ? opts.url : undefined;
+  this.ffts = opts.ffts ? opts.ffts : [];
+  this.gainNode = opts.gainNode ? opts.gainNode : undefined;
+}
+
+AudioSource.prototype = {
+  needBuffer: function() {
+    return this.buffer === undefined;
+  },
+  loadSound: function(url, cb) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.responseType = 'arraybuffer';
+    var self = this;
+    req.onloadend = function() {
+      self.decode.call(self, req.response, cb);
+    };
+    req.send();
+  },
+  getBuffer: function(cb) {
+    if (!this.needBuffer()) return;
+    var self = this;
+    this.loadSound(this.url, function(data) {
+      self.onLoaded.call(self, data, true);
+    });
+  },
+  getSource: function(cb) {
+    if (this.source) {
+      cb(this.source);
+    } else {
+      var self = this;
+      this.disconnect();
+      this.loadSound(this.url, function(data) {
+        this.source = self.createSource.call(self, data, true);
+        cb(this.source);
+      });
+    }
+  },
+
+  onLoaded: function(source, silent) {
+    this.buffer = source;
+    this.disconnect();
+    this.source = this.context.createBufferSource();
+    this.source.buffer = this.buffer;
+    this.source.connect(this.gainNode);
+    this.ffts.forEach(function(fft) {
+      this.gainNode.connect(fft.input);
+    }, this);
+    this.gainNode.connect(this.context.destination);
+    this.ffts.forEach(function(fft) {
+      fft.connect(this.context.destination);
+    }, this);
+    if (!silent) this.playSound();
+  },
+  disconnect: function() {
+    if (this.source) {
+      this.source.disconnect(this.context.destination);
+    }
+  },
+  playSound: function() {
+    this.source.start(this.context.currentTime);
+  },
+  loadSilent: function() {
+    if (!this.needBuffer()) return;
+    var self = this;
+    this.loadSound(this.url, function(data) {
+      self.onLoaded.call(self, data, true);
+    });
+  },
+  play: function() {
+    if (this.needBuffer()) {
+      var self = this;
+      this.loadSound(this.url, function(data) {
+        self.onLoaded.call(self, data);
+      });
+    } else {
+      this.onLoaded(this.buffer);
+    }
+  },
+  stop: function() {
+    this.source.stop(this.context.currentTime);
+  },
+  decode: function(data, success, error) {
+    this.context.decodeAudioData(data, success, error);
+  }
+};
+
+module.exports = AudioSource;
